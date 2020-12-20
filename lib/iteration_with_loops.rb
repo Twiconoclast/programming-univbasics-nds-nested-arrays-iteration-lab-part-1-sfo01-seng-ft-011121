d55009adffd869b285d3bc933622a560a0a3e6d1
Carrie Bares <caroltbares@gmail.com>
2020-12-20 21:40:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,9 @@
 def find_even_values(src)
   i = 0
+  
   while i < src.length
     i2 = 0
+    
     while i2 < src[i].length
       if src[i].even?
         puts src[i]
@@ -9,9 +11,6 @@ def find_even_values(src)
       i2 += 1
     end
   
-  
     i += 1
   end
-  
-
 end
